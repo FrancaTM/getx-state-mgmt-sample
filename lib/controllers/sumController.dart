c5fb0ef26d89527f00fcb97162c38359109d5dd9
FrancaTM <francatm@outlook.com>
2020-09-02 18:08:18 -0300

    add workers

--- lib/controllers/sumController.dart
+++ lib/controllers/sumController.dart
@@ -9,4 +9,16 @@ class SumController extends GetxController {
   increment2() => count2.value++;
 
   int get sum => count1.value + count2.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    ever(count1, (_) => print("count1 has been changed"));
+    once(count1, (_) => print("first time count1 is changed"));
+    debounce(count1, (_) => print("count1 hasn't changed for 1s"),
+        time: Duration(seconds: 1));
+    interval(count1, (_) => print("every 1s count1 is changed"),
+        time: Duration(seconds: 1));
+  }
 }
