6ee2de9d06269974382011331fdf8ee27fa0d322
FrancaTM <francatm@outlook.com>
2020-09-01 21:54:42 -0300

    if using Get for state mgmt only...

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:getx_state_mgmt_sample/screens/first.dart';
 
 void main() {
-  runApp(GetMaterialApp(
+  runApp(MaterialApp(
     home: First(),
   ));
 }
